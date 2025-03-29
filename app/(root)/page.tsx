475efe101a93506fdc3e755d69151d04a671e58f
aryansehrawat13 <sehrawataryan48@gmail.com>
2025-03-29 22:53:30 +0530

    show generated interviews on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,23 @@ import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {getCurrentUser, getInterviewsByUserId, getLatestInterviews} from "@/lib/actions/auth.action";
 
-const page = () => {
+const page = async () => {
+
+  const user = await getCurrentUser();
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id! })
+  ]);
+
+  
+
+  const hasPastInterview = userInterviews?.length >0;
+
+  const hasUpcomingInterviews = latestInterviews?.length >0;
+
   return (
     <>
       <section className='card-cta'>
@@ -23,18 +38,33 @@ const page = () => {
       <section className='flex flex-col gap-6 mt-8'>
           <h2>Your Interviews</h2>
           <div className='interviews-section'>
-              {dummyInterviews.map((interview) => (
-                  <InterviewCard {...interview} key={interview.id} />
-              ))}
+              {
+                  hasPastInterview ? (
+                    userInterviews?.map((interview) => (
+                      <InterviewCard {...interview} key={interview.id} />
+
+                    ))) : (
+
+                      <p>You haven&apos;t taken any interview yet</p>
+                    )
+                  }
+
           </div>
       </section>
 
       <section className='flex flex-col gap-6 mt-8'>
           <h2>Take an Interview</h2>
           <div className='interviews-section'>
-          {dummyInterviews.map((interview) => (
-                  <InterviewCard {...interview} key={interview.id} />
-              ))}
+          {
+                  hasUpcomingInterviews ? (
+                    latestInterviews?.map((interview) => (
+                      <InterviewCard {...interview} key={interview.id} />
+
+                    ))) : (
+
+                      <p>There are no new interviews available</p>
+                    )
+                  }
 
           {/* <p>You haven&apos;t taken any interviews yet</p> */}
           </div>
